6b720477936d227978ff5cf085d4681dc8141707
Mohammed Farhan Sayed <84790596+mfarhansayed@users.noreply.github.com>
2024-08-07 17:51:54 +0530

    Delete Backend directory

--- Backend/app/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+pymysql://user:password@db_endpoint/db_name')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- Backend/app/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class Person(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    role = db.Column(db.String(64), nullable=False)
