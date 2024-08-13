165667e0660cf1defbd0cd9e2e7512ed85ad02ec
mfarhansayed <farhansayed1116@gmail.com>
2024-08-14 01:07:13 +0530

    build-4

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return "Flask and AppRunner!"
+  return "Flask on AppRunner!"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
